1232bd84c1d42d9f967405bf4651081337648bd7
Talles Alves <talves2503@gmail.com>
2022-12-03 20:26:50 -0300

    edit and create functions for the habits

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from "react";
+
 import { useNavigation } from "@react-navigation/native";
 import { ScrollView, Text, View, StyleSheet } from "react-native";
 
 import LifeStatus from "../../components/common/lifeStatus";    
 import StatusBar from "../../components/home/statusBar";
 import CreateHabit from "../../components/home/createHabit"
+import EditHabit from "../../components/home/editHabit";
 
 export default function Home() {
     const navigation = useNavigation();
@@ -25,7 +27,65 @@ export default function Home() {
                         ❤️ 20 dias • ✅ 80 Checks
                         <LifeStatus/>
                         <StatusBar/>
-                        <CreateHabit habitArea="Mente" borderColor="#90b7f3"></CreateHabit>
+
+                        { mindHabit ? (
+                            <EditHabit
+                                habit={mindHabit?.habitName}
+                                frequency={`${mindHabit?.habitTime} - ${mindHabit?.habitFrequency}`}
+                                habitArea={mindHabit?.habitArea}
+                                checkColor="#90b7f3"
+                            />
+                        ) : (
+                            <CreateHabit
+                                habitArea={"Mente"}
+                                borderColor={"#90b7f3"}
+                            />
+                        )}
+
+                        { moneyHabit ? (
+                            <EditHabit
+                                habit={moneyHabit?.habitName}
+                                frequency={`${moneyHabit?.habitTime} - ${moneyHabit?.habitFrequency}`}
+                                habitArea={moneyHabit?.habitArea}
+                                checkColor="#85bb65"
+                            />
+                        ) : (
+                            <CreateHabit
+                                habitArea={"Financeiro"}
+                                borderColor={"#85bb65"}
+                            />
+                        )}
+
+                        { bodyHabit ? (
+                            <EditHabit
+                                habit={bodyHabit?.habitName}
+                                frequency={`${bodyHabit?.habitTime} - ${bodyHabit?.habitFrequency}`}
+                                habitArea={bodyHabit?.habitArea}
+                                checkColor="#ff0044"
+                            />
+                        ) : (
+                            <CreateHabit
+                                habitArea={"Corpo"}
+                                borderColor={"#ff0044"}
+                            />
+                        )}
+
+                        { funHabit ? (
+                            <EditHabit
+                                habit={funHabit?.habitName}
+                                frequency={`${funHabit?.habitTime} - ${funHabit?.habitFrequency}`}
+                                habitArea={funHabit?.habitArea}
+                                checkColor="#fe7f23"
+                            />
+                        ) : (
+                            <CreateHabit
+                                habitArea={"Humor"}
+                                borderColor={"#fe7f23"}
+                            />
+                        )}
+                            
+                            
+                        
                     </Text>
                 </View>
                 <Text 
@@ -34,7 +94,7 @@ export default function Home() {
                         handleNavExplanation();
                     }}
                 >
-                    Ver explicação novamente.
+                    Ver explicações novamente.
                 </Text>
             </ScrollView>
         </View>
