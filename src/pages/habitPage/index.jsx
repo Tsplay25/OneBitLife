200a9796bcc8273f190ac0cc2f85c44c449ccff6
Talles Alves <talves2503@gmail.com>
2022-12-03 23:10:41 -0300

    create habit and route structures

--- src/pages/habitPage/index.jsx
+++ src/pages/habitPage/index.jsx
@@ -3,8 +3,9 @@ import React from "react";
 
 import {  View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Alert } from "react-native"
 
-export default function HabitPage() {
+export default function HabitPage({ route }) {
     const navigation = useNavigation();
+    const { create, habit } = route.params;
 
     return (
         <View style={styles.container}>
@@ -19,7 +20,13 @@ export default function HabitPage() {
                             style={styles.arrowBack}
                         />
                     </TouchableOpacity>
-                    <View style={styles.mainContent}></View>
+                    <View style={styles.mainContent}>
+                        <Text style={styles.title}>Configurações {"\n"}de hábito</Text>
+                        <Text style={styles.inputText}>Área</Text>
+                        <View style={styles.inputContainer}>
+                            <Text style={styles.area}>{habit?.habitArea}</Text>
+                        </View>
+                    </View>
                 </View>
             </ScrollView>
         </View>
@@ -43,5 +50,29 @@ const styles = StyleSheet.create({
     mainContent: {
         width: 250,
         alignSelf: "center",
+    },
+    title: {
+        fontWeight: "bold",
+        textAlign: "center",
+        color: "#FFF",
+        fontSize: 30,
+    },
+    inputText: {
+        color: "#FFF",
+        fontSize: 16,
+        marginTop: 35,
+        marginBottom: 10,
+        marginLeft: 5,
+    },
+    inputContainer: {
+        borderWidth: 1,
+        borderColor: "#FFF",
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+    },
+    area: {
+        color: "#BBBBBB",
+        fontSize: 15,
     },
 })
