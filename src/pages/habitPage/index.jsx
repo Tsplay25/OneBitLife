08b1d02d5e66ddaf76819e4e1a588f5c0f1ae401
Talles Alves <talves2503@gmail.com>
2022-12-04 00:26:50 -0300

    dropdown list to select habit frequency

--- src/pages/habitPage/index.jsx
+++ src/pages/habitPage/index.jsx
@@ -3,10 +3,13 @@ import { useNavigation } from "@react-navigation/native";
 
 import {  View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Alert } from "react-native"
 import SelectHabit from "../../components/habitPage/selectHabit";
+import SelectFrequency from "../../components/habitPage/selectFrequency";
 
 export default function HabitPage({ route }) {
     const navigation = useNavigation();
     const [habitInput, setHabitInput] = useState();
+    const [frequencyInput, setFrequencyInput ] = useState();
+
     const { create, habit } = route.params;
 
     return (
@@ -30,6 +33,12 @@ export default function HabitPage({ route }) {
                         </View>
                         <Text style={styles.inputText}>Hábito</Text>
                         <SelectHabit habit={habit} habitInput={setHabitInput} />
+                        <Text style={styles.inputText}>Frequência</Text>
+                        <SelectFrequency
+                            habitFrequency={habit?.habitFrequency}
+                            frequencyInput={setFrequencyInput}
+                        />
+
                     </View>
                 </View>
             </ScrollView>
